a708d209319f4ab1afbbad3b4b3ceb9e073b7aa6
Matheus Santos <87808357+matheussantosspbr@users.noreply.github.com>
2022-07-15 16:11:39 -0300

    Add files via upload

---
+++ app/core/processCad.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once '../../lib/matheus/Database/config.php';
+
+if (!empty($_POST)) {
+  if (!empty($_POST['nome'])) {
+    if (!empty($_POST['sobrenome'])) {
+      if (!empty($_POST['datanasc'])) {
+        if (!empty($_POST['email']) && !empty($_POST['confEmail'])) {
+          if ($_POST['email'] == $_POST['confEmail']) {
+            if (!empty($_POST['confSenha']) && !empty($_POST['senha'])) {
+              if ($_POST['senha'] == $_POST['confSenha']) {
+                try {
+                  $sql = "INSERT INTO usuarios
+                              (nome, sobrenome,data_nascimento,email, senha)
+                            VALUES
+                              (:nome, :sobrenome, :datanasc,:email, :senha)";
+                  $stmt = $pdo->prepare($sql);
+
+                  $dados = array(
+                    ':nome' => $_POST['nome'],
+                    ':sobrenome' => $_POST['sobrenome'],
+                    ':datanasc' => $_POST['datanasc'],
+                    ':email' => $_POST['email'],
+                    ':senha' => md5($_POST['senha']),
+                  );
+                  if ($stmt->execute($dados)) {
+                    header("Location: ../indexCad.php?msgSucesso=Cadastro realizado com sucesso");
+                  } else {
+                    header("Location: ../indexCad.php?msgErro=Erro de Acesso");
+                  }
+                } catch (PDOException $e) {
+                  header("Location: ../indexCad.php?msgErro=Email já Existente !");
+                }
+              } else {
+                header("Location: ../indexCad.php?msgErro=As senhas não se coincidem");
+              }
+            } else {
+              header("Location: ../indexCad.php?msgErro=Os Campos de Senhas não pode estar vazias !");
+            }
+          } else {
+            header("Location: ../indexCad.php?msgErro=Os emails não se coincidem");
+          }
+        } else {
+          header("Location: ../indexCad.php?msgErro=Os Campos de Emails não pode estar vazios !");
+        }
+      } else {
+        header("Location: ../indexCad.php?msgErro=Coloque sua data de nascimento !");
+      }
+    } else {
+      header("Location: ../indexCad.php?msgErro=O Campo sobrenome não pode estar vazio !");
+    }
+  } else {
+    header("Location: ../indexCad.php?msgErro=O Campo nome não pode estar vazio !");
+  }
+} else {
+  header("Location: ../indexCad.php?msgErro=Erro");
+}
