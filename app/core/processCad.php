22a293b62a7923f8d29ad91e2ceeae06071e88cc
Matheus Santos <87808357+matheussantosspbr@users.noreply.github.com>
2022-07-27 20:31:05 -0300

    #1

--- app/core/processCad.php
+++ app/core/processCad.php
@@ -10,27 +10,32 @@ if (!empty($_POST)) {
           if ($_POST['email'] == $_POST['confEmail']) {
             if (!empty($_POST['confSenha']) && !empty($_POST['senha'])) {
               if ($_POST['senha'] == $_POST['confSenha']) {
-                try {
-                  $sql = "INSERT INTO usuarios
-                              (nome, sobrenome,data_nascimento,email, senha)
-                            VALUES
-                              (:nome, :sobrenome, :datanasc,:email, :senha)";
-                  $stmt = $pdo->prepare($sql);
+                if (!empty($_POST['insertImg'])) {
+                  try {
+                    $sql = "INSERT INTO usuarios
+                                (nome, sobrenome,data_nascimento,email, senha, foto_perfil)
+                              VALUES
+                                (:nome, :sobrenome, :datanasc,:email, :senha, :insertImg)";
+                    $stmt = $pdo->prepare($sql);
 
-                  $dados = array(
-                    ':nome' => $_POST['nome'],
-                    ':sobrenome' => $_POST['sobrenome'],
-                    ':datanasc' => $_POST['datanasc'],
-                    ':email' => $_POST['email'],
-                    ':senha' => md5($_POST['senha']),
-                  );
-                  if ($stmt->execute($dados)) {
-                    header("Location: ../indexCad.php?msgSucesso=Cadastro realizado com sucesso");
-                  } else {
-                    header("Location: ../indexCad.php?msgErro=Erro de Acesso");
+                    $dados = array(
+                      ':nome' => $_POST['nome'],
+                      ':sobrenome' => $_POST['sobrenome'],
+                      ':datanasc' => $_POST['datanasc'],
+                      ':email' => $_POST['email'],
+                      ':senha' => md5($_POST['senha']),
+                      ':insertImg' => ($_FILES['insertImg'])
+                    );
+                    if ($stmt->execute($dados)) {
+                      header("Location: ../../index.php?msgSucesso=Cadastro realizado com sucesso");
+                    } else {
+                      header("Location: ../indexCad.php?msgErro=Erro de Acesso");
+                    }
+                  } catch (PDOException $e) {
+                    header("Location: ../indexCad.php?msgErro=Email já Existente !");
                   }
-                } catch (PDOException $e) {
-                  header("Location: ../indexCad.php?msgErro=Email já Existente !");
+                } else {
+                  header("Location: ../indexCad.php?msgErro=Insira uma Foto de Perfil");
                 }
               } else {
                 header("Location: ../indexCad.php?msgErro=As senhas não se coincidem");
