01954f698cedda280bd7b7674455629792fb9d32
Matheus Santos <87808357+matheussantosspbr@users.noreply.github.com>
2022-07-27 22:12:10 -0300

    #1

--- app/core/processCad.php
+++ app/core/processCad.php
@@ -14,34 +14,31 @@ if (!empty($_POST)) {
               // verificação se os campos senha e confirmação de senha é iguais
               if ($_POST['senha'] == $_POST['confSenha']) {
                 // se estiver tudo certo, fazer o processo de cadastro
-                  try {
-                    $sql = "INSERT INTO usuarios
+                try {
+                  $sql = "INSERT INTO usuarios
                                 (nome, sobrenome,data_nascimento,email, senha, foto_perfil)
                               VALUES
                                 (:nome, :sobrenome, :datanasc,:email, :senha, :insertImg)";
-                    $stmt = $pdo->prepare($sql);
+                  $stmt = $pdo->prepare($sql);
 
-                    $dados = array(
-                      ':nome' => $_POST['nome'],
-                      ':sobrenome' => $_POST['sobrenome'],
-                      ':datanasc' => $_POST['datanasc'],
-                      ':email' => $_POST['email'],
-                      ':senha' => md5($_POST['senha']),
-                      ':insertImg' => ($_FILES['insertImg'])
-                    );
-                    // execultar cadastro
-                    if ($stmt->execute($dados)) {
-                      header("Location: ../../index.php?msgSucesso=Cadastro realizado com sucesso");
-                    } else {
-                      header("Location: ../indexCad.php?msgErro=Erro de Acesso");
-                    }
-                    
-                    // caso der erro, siginifica que o email já existe
-                  } catch (PDOException $e) {
-                    header("Location: ../indexCad.php?msgErro=Email já Existente !");
+                  $dados = array(
+                    ':nome' => $_POST['nome'],
+                    ':sobrenome' => $_POST['sobrenome'],
+                    ':datanasc' => $_POST['datanasc'],
+                    ':email' => $_POST['email'],
+                    ':senha' => md5($_POST['senha']),
+                    ':insertImg' => ($_FILES['insertImg'])
+                  );
+                  // execultar cadastro
+                  if ($stmt->execute($dados)) {
+                    header("Location: ../../index.php?msgSucesso=Cadastro realizado com sucesso");
+                  } else {
+                    header("Location: ../indexCad.php?msgErro=Erro de Acesso");
                   }
-                } else {
-                  header("Location: ../indexCad.php?msgErro=Insira uma Foto de Perfil");
+
+                  // caso der erro, siginifica que o email já existe
+                } catch (PDOException $e) {
+                  header("Location: ../indexCad.php?msgErro=Email já Existente !");
                 }
               } else {
                 header("Location: ../indexCad.php?msgErro=As senhas não se coincidem");
