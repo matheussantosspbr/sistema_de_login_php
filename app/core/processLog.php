0877930be373e5d694d7e744f4cf428fffa52a6d
Matheus Santos <87808357+matheussantosspbr@users.noreply.github.com>
2022-08-08 18:18:53 -0300

    Update processLog.php

--- app/core/processLog.php
+++ app/core/processLog.php
@@ -2,32 +2,36 @@
 session_start();
 require_once '../../lib/matheus/Database/config.php';
 
+
 if (!empty($_POST)) {
-  // verificação se os campos email e senha está vazia
   if (!empty($_POST['email'])) {
     if (!empty($_POST['senha'])) {
-      $email = $_POST['email'];
-      $senha = md5($_POST['senha']);
+      try {
+        $email = $_POST['email'];
+        $senha = md5($_POST['senha']);
 
-      $stmt = $pdo->prepare(
-        "SELECT * FROM usuarios WHERE email = ? AND senha = ?"
-      );
+        $stmt = $pdo->prepare(
+          "SELECT * FROM usuarios WHERE email = ? AND senha = ?"
+        );
 
-      $stmt->bindParam(1, $email);
-      $stmt->bindParam(2, $senha);
+        $stmt->bindParam(1, $email);
+        $stmt->bindParam(2, $senha);
 
-      $stmt->execute();
-      $users = $stmt->fetchAll();
+        $stmt->execute();
+        $users = $stmt->fetchAll();
 
 
-      if (!($users == array())) {
-        $_SESSION['usuario'] = $email;
-        $_SESSION['senha'] = $senha;
-        header("Location: ../painel.php");
-        exit();
-      } else {
-        header("Location: ../../index.php?msgErro=Email ou Senha invalida !");
-        exit();
+        if (!($users == array())) {
+          $_SESSION['usuario'] = $email;
+          $_SESSION['senha'] = $senha;
+          header("Location: ../painel.php");
+          exit();
+        } else {
+          header("Location: ../../index.php?msgErro=Email ou Senha invalida !");
+          exit();
+        }
+      } catch (\Throwable $e) {
+        header("Location: ../../index.php?msgErro=Erro !");
       }
     } else {
       header("Location: ../../index.php?msgErro=Digite sua Senha !");
